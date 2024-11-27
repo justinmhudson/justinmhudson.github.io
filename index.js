280069c1aa683559dcf458916708f44769d7bdd5
justinmhudson <jhud2010@gmail.com>
2024-11-27 07:16:32 -0800

    fetch json in js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function myFunction() {
     document.getElementById("demo").innerHTML = "Paragraph changed.";
-  }
+}
+
+function fetchJSONData() {
+    fetch('./testdata.json')
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}
