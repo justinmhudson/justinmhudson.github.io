a821e87e970fdf3397ec0a44c82461b5cba31990
justinmhudson <jhud2010@gmail.com>
2024-11-27 07:30:17 -0800

    start table js on load

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Paragraph changed.";
-}
-
 function fetchJSONData() {
     fetch('./testdata.json')
     .then((response) => response.json())
     .then((json) => console.log(json));
 }
+
+window.addEventListener('load', convert);
 
 function convert() {
          
